58e0818cff4f9602ca548fd5a887c64f30aa1cd7
harrilam <56561492+harrilam@users.noreply.github.com>
2019-11-28 19:35:21 +0000

    css spinning image

--- index.js
+++ index.js
@@ -3,8 +3,28 @@ function greet () {
   alert(greeting)
 }
 
-let button = document.querySelector(".btn-outline-americano")
-button.addEventListener("click", greet)
+// let button = document.querySelector(".btn-outline-americano")
+// button.addEventListener("click", greet)
+//
+// let latteButton = document.querySelector("#latte")
+// button.addEventListener("click", latte.html)
 
-let latteButton = document.querySelector("#latte")
-button.addEventListener("click", latte.html)
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+document.getElementById("rotator");
+  let timeoutID = scope.setTimeout(function, "click");
